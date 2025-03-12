8b18ac6c43a871bb687c2e0a279b1d223c87e8cf
Huy <huyluong82@googlemail.com>
2025-03-12 09:00:26 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: true,
-
   modules: [
     "@nuxt/content",
     "@nuxt/image",
@@ -11,16 +10,13 @@ export default defineNuxtConfig({
     "shadcn-nuxt",
     // "nuxt-security",
   ],
-
   shadcn: {
     prefix: "",
     componentDir: "./components/ui",
   },
-
   routeRules: {
-    "/": { prerender: false },
+    "/": { prerender: true },
   },
-
   content: {
     build: {
       markdown: {
@@ -33,15 +29,12 @@ export default defineNuxtConfig({
       api: "https://api.nuxt.studio",
     },
   },
-
   image: {
     format: ["webp"],
   },
-
   tailwindcss: {
     cssPath: "~/assets/css/main.css",
   },
-
   // site: {
   //   url: "https://ai.293979.xyz",
   //   name: "My Awesome Site",
@@ -49,6 +42,8 @@ export default defineNuxtConfig({
   //     "Welcome to my awesome site where you can find interesting stuff about coding, electronics and more.",
   //   defaultLocale: "en",
   // },
-
+  future: {
+    compatibilityVersion: 4,
+  },
   compatibilityDate: "2025-03-11",
 });
